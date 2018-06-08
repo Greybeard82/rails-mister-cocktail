15743cf907f7036c052daa6e781050d08cf414c7
David Martins <davidvicentemartins@gmail.com>
2018-06-08 12:52:33 +0200

    Backend running, rake still red but everything works

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -6,15 +6,19 @@ class DosesController < ApplicationController
   end
 
   def create
-    @cocktail = cocktail.find(params[:cocktail_id])
-    @dose = Dose.new(params[:id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.save
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   def destroy
-    @dose = Dose.destroy(params[:id])
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   def dose_params
-    params.require(:dose).permit(:ingredient_id)
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
   end
 end
